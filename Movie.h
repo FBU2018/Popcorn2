edaa454ab932dcddb59386d61cf031f86dfb9be9
Ernest Omondi <eomondi@fb.com>
2018-07-16 15:38:27 -0700

    Finished barebones search functionality and created Movie NSObject

---
+++ Movie.h
@@ -0,0 +1,13 @@
+//
+//  Movie.h
+//  Popcorn
+//
+//  Created by Ernest Omondi on 7/16/18.
+//  Copyright © 2018 Rucha Patki. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Movie : NSObject
+
+@end
---
+++ Movie.m
@@ -0,0 +1,13 @@
+//
+//  Movie.m
+//  Popcorn
+//
+//  Created by Ernest Omondi on 7/16/18.
+//  Copyright © 2018 Rucha Patki. All rights reserved.
+//
+
+#import "Movie.h"
+
+@implementation Movie
+
+@end
