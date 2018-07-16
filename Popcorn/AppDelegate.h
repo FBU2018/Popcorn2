679b596a6bd1ace64e9450b622d76698b362a10f
Rucha Patki <ruchapatki@fb.com>
2018-07-16 10:17:01 -0700

    Initial Commit

---
+++ Popcorn/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Popcorn
+//
+//  Created by Rucha Patki on 7/16/18.
+//  Copyright © 2018 Rucha Patki. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Popcorn/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Popcorn
+//
+//  Created by Rucha Patki on 7/16/18.
+//  Copyright © 2018 Rucha Patki. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
