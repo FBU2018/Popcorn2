219e637adeee696c0390c2fbb2f238e53489d3dc
Ernest Omondi <eomondi@fb.com>
2018-07-18 14:15:40 -0700

    Implemented continuous search results

--- Popcorn/View Controllers/PCSearchViewController.m
+++ Popcorn/View Controllers/PCSearchViewController.m
@@ -19,6 +19,8 @@
 @property (strong, nonatomic) NSArray *filteredMovieObjects;
 @property (strong, nonatomic) IBOutlet UISearchBar *searchBar;
 @property (strong, nonatomic) NSArray *moviesArray;
+-(void) searchMoviesWithString:(NSString *)searchString andCompletionHandler:(void (^) (NSArray*))completionHandler;
+-(void)searchAndFilterWithSearchString:(NSString *) searchText;
 @end
 
 @implementation PCSearchViewController
@@ -66,7 +68,49 @@
     return cell;
 }
 
-- (void) searchMoviesWithString: (NSString*) searchString{
+//- (void) searchMoviesWithString: (NSString*) searchString{
+//    // place search string in URL
+//    NSString *baseUrl = @"https://api.themoviedb.org/3/search/movie?api_key=69308a1aa1f4a3c54b17a53c591eadb0&language=en-US&query=";
+//    NSString *searchUrl = [baseUrl stringByAppendingString:searchString];
+//    NSString *fullUrl = [searchUrl stringByAppendingString:@"&page=1&include_adult=false"];
+//
+//    NSURL *url = [NSURL URLWithString:fullUrl];
+//
+//    NSURLRequest *request = [NSURLRequest requestWithURL:url cachePolicy:NSURLRequestReloadIgnoringLocalCacheData timeoutInterval:10.0];
+//    NSURLSession *session = [NSURLSession sessionWithConfiguration:[NSURLSessionConfiguration defaultSessionConfiguration] delegate:nil delegateQueue:[NSOperationQueue mainQueue]];
+//    NSURLSessionDataTask *task = [session dataTaskWithRequest:request completionHandler:^(NSData * _Nullable data, NSURLResponse * _Nullable response, NSError * _Nullable error) {
+//        // this runs when network call is finished
+//        if (error != nil) {
+//            //creating alert if networking error
+//            UIAlertController *alert = [UIAlertController alertControllerWithTitle:@"Cannot Get Movies" message:@"The internet connection appears to be offline." preferredStyle:(UIAlertControllerStyleAlert)];
+//
+//            // create try again action
+//            UIAlertAction *tryAgainAction = [UIAlertAction actionWithTitle:@"Try Again" style:UIAlertActionStyleDefault handler:^(UIAlertAction * _Nonnull action) {
+//                // handle tryAgain response here. Doing nothing will dismiss the view.
+//                [self searchMoviesWithString:searchString];
+//            }];
+//            [self presentViewController:alert animated:YES completion:^{
+//                // optional code for what happens after the alert controller has finished presenting
+//            }];
+//            // add the tryAgain action to the alertController
+//            [alert addAction:tryAgainAction];
+//
+//            NSLog(@"%@", [error localizedDescription]);
+//        }
+//        else{
+//            // store the received data in a dictionary
+//            NSDictionary *dataDictionary = [NSJSONSerialization JSONObjectWithData:data options:NSJSONReadingMutableContainers error:nil];
+//            // store the results section of the JSON data in the movies array
+//            NSArray *dictionaries = dataDictionary[@"results"];
+//
+//            self.moviesArray = dictionaries;
+//        }
+//    }];
+//    [task resume];
+//}
+
+// define a completion block for the search network call
+-(void) searchMoviesWithString:(NSString *)searchString andCompletionHandler:(void (^) (NSArray*))completionHandler{
     // place search string in URL
     NSString *baseUrl = @"https://api.themoviedb.org/3/search/movie?api_key=69308a1aa1f4a3c54b17a53c591eadb0&language=en-US&query=";
     NSString *searchUrl = [baseUrl stringByAppendingString:searchString];
@@ -85,7 +129,7 @@
             // create try again action
             UIAlertAction *tryAgainAction = [UIAlertAction actionWithTitle:@"Try Again" style:UIAlertActionStyleDefault handler:^(UIAlertAction * _Nonnull action) {
                 // handle tryAgain response here. Doing nothing will dismiss the view.
-                [self searchMoviesWithString:searchString];
+                [self searchMoviesWithString:searchString andCompletionHandler:nil];
             }];
             [self presentViewController:alert animated:YES completion:^{
                 // optional code for what happens after the alert controller has finished presenting
@@ -96,67 +140,60 @@
             NSLog(@"%@", [error localizedDescription]);
         }
         else{
+            NSLog(@"Network call returned results");
             // store the received data in a dictionary
             NSDictionary *dataDictionary = [NSJSONSerialization JSONObjectWithData:data options:NSJSONReadingMutableContainers error:nil];
             // store the results section of the JSON data in the movies array
             NSArray *dictionaries = dataDictionary[@"results"];
             
             self.moviesArray = dictionaries;
+            completionHandler(self.moviesArray);
         }
     }];
     [task resume];
 }
 
-// called when user starts typing
-- (void)searchBar:(UISearchBar *)searchBar textDidChange:(NSString *)searchText{
-    if (searchText.length != 0) {
-        // network call to get the search results
-        [self searchMoviesWithString:searchText];
-        
-        // set up predicate for searching movies
-        NSPredicate *predicate = [NSPredicate predicateWithBlock:^BOOL(NSDictionary *item, NSDictionary *bindings) {
-            return [item[@"title"] containsString:searchText];
+// helper function that filters network call results with search query
+-(void)searchAndFilterWithSearchString:(NSString *) searchText{
+    if ([searchText isEqualToString:@""]){
+        self.filteredData = nil;
+        [self.searchTableView reloadData];
+    }
+    else if (searchText.length != 0) {
+        // network call to get the search results with a completion handler
+        [self searchMoviesWithString:searchText andCompletionHandler:^(NSArray *results) {
+            // set up predicate for searching movies
+            NSPredicate *predicate = [NSPredicate predicateWithBlock:^BOOL(NSDictionary *item, NSDictionary *bindings) {
+                return [item[@"title"] containsString:searchText];
+            }];
+            
+            self.filteredData = [results filteredArrayUsingPredicate:predicate];
+            [self.searchTableView reloadData];
+            
         }];
-        
-        self.filteredData = [self.moviesArray filteredArrayUsingPredicate:predicate];
     }
     // if user hasn't typed anything then don't filter movies
     else {
         self.filteredData = self.moviesArray;
     }
-    
-    [self.searchTableView reloadData];
 }
 
-//// called when keyboard search button pressed
-//- (void)searchBarSearchButtonClicked:(UISearchBar *)searchBar{
-//    // String to store search text
-//    NSString *searchText = searchBar.text;
-//    
-//    // network call to get the search results
-//    [self searchMoviesWithString:searchText];
-//    
-//    if (searchText.length != 0) {
-//        NSPredicate *predicate = [NSPredicate predicateWithBlock:^BOOL(NSDictionary *item, NSDictionary *bindings) {
-//            return [item[@"title"] containsString:searchText];
-//        }];
-//        
-//        self.filteredData = [self.moviesArray filteredArrayUsingPredicate:predicate];
-//    }
-//    else {
-//        self.filteredData = self.moviesArray;
-//    }
-//    
-//    [self.searchTableView reloadData];
-//    
-//}
+// called when user starts typing
+- (void)searchBar:(UISearchBar *)searchBar textDidChange:(NSString *)searchText{
+    [self searchAndFilterWithSearchString:searchText];
+}
+
+// called when keyboard search button pressed
+- (void)searchBarSearchButtonClicked:(UISearchBar *)searchBar{
+    [self searchAndFilterWithSearchString:searchBar.text];
+}
 
 - (void)searchBarTextDidBeginEditing:(UISearchBar *)searchBar {
     self.searchBar.showsCancelButton = YES;
 }
 
 - (void)searchBarCancelButtonClicked:(UISearchBar *)searchBar {
-    self.filteredData = self.moviesArray;
+    self.filteredData = nil;
     [self.searchTableView reloadData];
     
     self.searchBar.showsCancelButton = NO;
