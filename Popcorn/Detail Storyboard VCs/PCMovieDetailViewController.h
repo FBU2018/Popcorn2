7de532f7215c52a8ed806655eab36bb624ade6f0
Sarah Embry <skembry@fb.com>
2018-07-27 16:32:01 -0700

    cleaning up unnecessary VC

--- Popcorn/Detail Storyboard VCs/PCMovieDetailViewController.h
+++ Popcorn/Detail Storyboard VCs/PCMovieDetailViewController.h
@@ -8,7 +8,6 @@
 
 #import <UIKit/UIKit.h>
 #import "Movie.h"
-#import "PCRatingViewController.h"
 
 @interface PCMovieDetailViewController : UIViewController 
 @property (strong, nonatomic) Movie *movie;
